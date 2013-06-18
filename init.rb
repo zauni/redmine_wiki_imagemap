3c8b6fdf0ecdd9c8c6a057660efbb54b96119951
Matthias Zaunseder <matthias.zaunseder@hotmail.de>
2013-06-18 11:33:45 +0200

    Image doesn't have to be a full URL and page doesn't have to be locked
    Now you don't have to insert the whole image URL. It will be fetched
    from the Attachment class.
    It is not necessary anymore to lock the wiki page.

--- init.rb
+++ init.rb
@@ -9,10 +9,10 @@ Redmine::Plugin.register :imagemapplugin do
   author 'Matthias Zaunseder'
   author_url 'http://github.com/zauni/redmine_wiki_imagemap'
   description 'Allows you to embed Imagemaps in your wiki.'
-  version '0.1.0'
+  version '0.2.0'
 
   Redmine::WikiFormatting::Macros.register do
-    desc "Embed image map\nUsage:\n<pre>{{imagemap (image_url)\nMap areas - everything between <map></map>\n}}</pre>"
+    desc "Embed image map\nUsage:\n<pre>{{imagemap(image_url)\nMap areas - everything between <map></map>\n}}</pre>"
     macro :imagemap do |obj, args, text|
       page = obj.page
       raise 'Page not found' if page.nil?
@@ -20,13 +20,14 @@ Redmine::Plugin.register :imagemapplugin do
       filename = args.first
       $currentmap.nil? ? ($currentmap = 1) : ($currentmap = $currentmap + 1)
 
-      # For security, only allow insertion on protected (locked) wiki pages
-      if page.protected
-        result = "<div style='overflow: auto; max-width: 100%;'><img src='#{filename}' usemap='#map-#{$currentmap}' /><map name='map-#{$currentmap}'>#{ CGI::unescapeHTML(text) }</map></div>".html_safe
-        return result
+      if obj && obj.respond_to?(:attachments) && attachment = Attachment.latest_attach(obj.attachments, filename)
+        imgurl = download_named_attachment_path(attachment, attachment.filename, :only_path => true)
+        img = image_tag(imgurl, :alt => attachment.description, :usemap => "#map-#{$currentmap}")
       else
-        return "<!-- Macro removed due to wiki page being unprotected -->"
+        raise "Attachment #{filename} not found"
       end
+
+      return "<div style='overflow: auto; max-width: 100%;'>#{img}<map name='map-#{$currentmap}'>#{ CGI::unescapeHTML(text) }</map></div>".html_safe
     end
   end
 
